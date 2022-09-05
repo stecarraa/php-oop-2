fc164dbb5558835cf91249701425d89b3b682bd1
stecarraa <stefano@caratozzolo.me>
2022-09-05 22:39:07 +0200

    feat= check email and discount

--- classes/user.php
+++ classes/user.php
@@ -6,6 +6,10 @@ class user{
     public $surname;
     protected $email;
 
+    public $discount = 0 ;
+
+    protected $registeredUser = false;
+
     public function __construct($_name, $_surname, $_email)
     {
         
@@ -20,13 +24,31 @@ class user{
         return $this->email;
     }
 
+    public function getRegisteredUser(){
+        return $this->registeredUser;
+    }
+
     public function setEmail($_email) {
-        
+
+        if (strpos($_email, '@') !== false && strpos($_email, '.') !== false) {
             $this->email = $_email;
-     
+        } else {
+            echo "Check email, not valid";
+        }
 
     }
 
+    public function setRegisteredUser($_registeredUser){
+        $this->registeredUser = $_registeredUser;
+
+}
+
+public function applyLoggedDiscount(){
+    if($this->registeredUser === true){
+         $discount = 20;
+        }
+
+}
 }
 
 ?>
